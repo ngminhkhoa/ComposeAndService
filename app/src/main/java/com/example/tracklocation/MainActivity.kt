08300792a6ea2ca48c622858d812add4a8f1c4fe
na61er <ybn.ca.khoa612009k@gmail.com>
2023-04-16 18:15:52 +0700

    Track Location Test

--- app/src/main/java/com/example/tracklocation/MainActivity.kt
+++ app/src/main/java/com/example/tracklocation/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.tracklocation
 import android.Manifest
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +16,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,13 +36,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             TrackLocationTheme {
                 Column(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
 
                     Button(onClick = {
                         Intent(applicationContext, LocationService::class.java).apply {
                             action = LocationService.START_ACTION
                             startService(this)
+                            Toast.makeText(applicationContext, "Start Tracking", Toast.LENGTH_SHORT).show()
                         }
                     }) {
                         Text(text = "Start")
@@ -51,6 +57,7 @@ class MainActivity : ComponentActivity() {
                             action = LocationService.STOP_ACTION
                             startService(this)
                         }
+                        Toast.makeText(applicationContext, "Stop Tracking", Toast.LENGTH_SHORT).show()
                     }) {
                         Text(text = "Stop")
                     }
